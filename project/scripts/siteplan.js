9900cb98696bbb9d72ee59b0cb5c7eec5e7b534e
nabendeandrew1955 <nmafabi@byupathway.edu>
2026-08-01 11:51:53 +0300

    created site page

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,4 @@
+let day = new Date();
+document.getElementById("year").innerHTML = `&copy; ${day.getFullYear()}`;
+document.querySelector("#lastmodified").textContent = `lastmodification: ${document.lastModified}`;
+
